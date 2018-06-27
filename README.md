949f5f6f32bdaf5b07730c4d445f325c407b663c
Nick Rogers and Swati Sahay <swati@>
2018-06-27 10:43:58 -0700

    Add to do list front end.

---
+++ README.md
@@ -0,0 +1,39 @@
+# _ToDoList_
+
+#### _A Website that is a to do list, June 27, 2018_
+
+#### By _**Nick Rogers and Swati Sahay**_
+
+## Description
+
+_This website was created to make to do lists.
+Specs:
+1. Welcome user with a greeting
+2. Allow user to choose from add or view the list
+3. Allow them to add item to list when they choose add and confirm
+4. Allow them to view item from list when they choose view and confirm
+5. Tell user if the list is empty
+_
+
+## Setup/Installation Requirements
+
+* _Double click the icon in your folder_
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_Nick Rogers email: amellocello@msn.com_
+_Swati Sahay email: swati27888@gmail.com_
+
+## Technologies Used
+
+_Created with Atom editor.  Use any browser to view._
+
+### License
+
+*No license required*
+
+Copyright (c) 2018 **_Nick Rogers and Swati Sahay_**
--- ToDoList/Models/ToDoList.cs
+++ ToDoList/Models/ToDoList.cs
@@ -1,15 +1,82 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList;
-
-namespace ToDoList.Tests
+using System.Collections.Generic;
+using System;
+namespace ToDoList.Models
 {
-  [TestClass]
-  public class ToDoListTest
+  public class Item
   {
-    [TestMethod]
-    public void ExampleTest_True()
+
+    private string _description;
+    private static List<Item> _instances = new List<Item> {};
+
+    public Item (string description)
     {
-      // eventually your testing code will go here
+      _description = description;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<Item> GetAll()
+    {
+       return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      bool IsDone= true;
+      // Item newItem = new Item(description);
+      Console.WriteLine ("Welcome to todo list, you can add anything you want");
+
+      while (IsDone == true)
+      {
+
+
+        Console.WriteLine("You want to add an item or view the list? select one (Add/View/Exit)");
+
+        string description = Console.ReadLine();
+        if (description=="add")
+        {
+          Console.WriteLine("Please enter your item ");
+          string newItems= Console.ReadLine();
+          Item newItem = new Item(newItems);
+          newItem.Save();
+          // Main(); This also works if you comment out while loop
+        }
+        else if(description=="view")
+        {
+          Console.WriteLine("You have these items in your to do list");
+          List<Item> result = Item.GetAll();
+          foreach (Item thisItem in result)
+          {
+            Console.WriteLine("List items: " + thisItem.GetDescription());
+          }
+          // Main();
+        }
+        else if(description == "exit")
+        {
+          Console.WriteLine("Goodbye");
+          IsDone = false;
+        }
+      }
+      // else
+      // {
+      //   Main();
+      // }
     }
   }
 }
