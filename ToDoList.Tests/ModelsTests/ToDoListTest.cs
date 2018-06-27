b37d80b5ed894868b48d8b6f45d0f8a504609050
Nick Rogers and Swati Sahay <swati@>
2018-06-27 08:14:07 -0700

    Create Initial Project Folder.

---
+++ ToDoList.Tests/ModelsTests/ToDoListTest.cs
@@ -0,0 +1,11 @@
+namespace ToDoList
+{
+public class ToDoList
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ToDoListTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
